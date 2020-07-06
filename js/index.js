cafe34cb9cc8ca5f47d8cb74ba224bf36044ce9d
Dee <47228065+DeeDowns@users.noreply.github.com>
2020-07-06 16:29:24 -0400

    added text for text-content sections and added middle image

--- js/index.js
+++ js/index.js
@@ -41,6 +41,8 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+
+//Nav Links
 const navLinks = document.querySelectorAll('nav a');
 console.log(navLinks)
 const serviceLink = navLinks[0].textContent = 'Services'
@@ -50,27 +52,62 @@ const featuresLink = navLinks[3].textContent = 'Features'
 const aboutLink = navLinks[4].textContent = 'About'
 const contactLink = navLinks[5].textContent = 'Contact'
 
+//Logo 
 const logoImg = document.querySelector('#logo-img');
 console.log(logoImg)
 logoImg.src = 'img/logo.png'
 
+//Header Image
 const headerImg = document.querySelector('#cta-img');
 console.log(headerImg)
 headerImg.src = 'img/header-img.png'
 
+//Header H1 Title
 const headerTitle = document.querySelector('.cta-text h1')
 console.log(headerTitle)
 headerTitle.innerHTML = 'DOM<br> Is<br> Awesome'
 
+//Header Button
 const buttonText = document.querySelector('.cta-text button')
 console.log(buttonText);
 buttonText.textContent = 'Get Started'
 
+//Saving all H4 tags to a variable
 const fourHeadings = document.querySelectorAll('.text-content h4');
 console.log(fourHeadings)
 
+//Assiging each h4 tag a variable and inserting text
 const features = fourHeadings[0].textContent = 'Features'
 const about = fourHeadings[1].textContent = 'About'
 const services = fourHeadings[2].textContent = 'Services'
 const product = fourHeadings[3].textContent = 'Product'
 const vision = fourHeadings[4].textContent = 'Vision'
+
+//adding h4 p text
+const textContentPs = document.querySelectorAll('.text-content p')
+console.log(textContentPs)
+
+const featuresTextContent = textContentPs[0].textContent = `Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`
+
+const aboutTextContent = textContentPs[1].textContent = `About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`
+
+const servicestTextContent = textContentPs[2].textContent = `Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`
+
+const productTextContent = textContentPs[3].textContent = `Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`
+
+const visionTextContent = textContentPs[4].textContent = `Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`
+
+//Middle Image
+const middleImg = document.querySelector('#middle-img');
+console.log(middleImg);
+middleImg.src = 'img/mid-page-accent.jpg'
